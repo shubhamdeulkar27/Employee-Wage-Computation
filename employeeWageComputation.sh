af33448b87e158871b979b6ea8bdbda82a2634ce
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-24 15:44:35 +0530

    Added: Calculating Daily Employee Wage

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -2,10 +2,14 @@
 
 #CHECKING EMPLOYEE IS PRESENT
 IS_PRESENT=1
+EMPLOYEE_WAGE_PER_HOUR=20
+FULL_DAY_HOURS=8
+
 checkEmployee=$(( RANDOM%2 ))
+dailyWage=0
 if (( $checkEmployee==$IS_PRESENT ))
 then
-	printf "Employee Is Present\n"
+	dailyWage=$(( $FULL_DAY_HOURS*$EMPLOYEE_WAGE_PER_HOUR ))
 else
 	printf "Employee Is Not Present\n"
 fi
