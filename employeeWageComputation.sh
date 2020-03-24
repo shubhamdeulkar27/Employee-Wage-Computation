048d158a380bb756033e20b8cedcc20c05421edd
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-24 17:24:44 +0530

    Refactored: Finds the Wages For A Month

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,27 +1,35 @@
 #!/bin/bash -x
 
-#CHECKING EMPLOYEE IS PRESENT
+#CONSTANTS
 IS_ABSENT=0
 IS_PART_TIME=1
 IS_FULL_TIME=2
 EMPLOYEE_WAGE_PER_HOUR=20
-checkEmployee=$(( RANDOM%3 ))
-dailyWage=0
+NUMBER_OF_WORKING_DAYS=20
 
-#CALCULATING DAILY WAGE
-case $checkEmployee in
-	$IS_FULL_TIME)
-		EMPLOYEE_HOURS=8
-	;;
-	$IS_PART_TIME)
-		EMPLOYEE_HOURS=4
-	;;
-	$IS_ABSENT)
-		EMPLOYEE_HOURS=0
-	;;
-	*)
-		printf "Invalid"
-	;;
-esac
+#VARIABLES
+monthlyWage=0
 
-dailyWage=$(( $EMPLOYEE_HOURS*$EMPLOYEE_WAGE_PER_HOOUR ))
+for (( day=1 ; day<=$NUMBER_OF_WORKING_DAYS ; day++ ))
+do
+	checkEmployee=$(( RANDOM%3 ))
+	dailyWage=0
+
+	#CALCULATING DAILY WAGE
+	case $checkEmployee in
+		$IS_FULL_TIME)
+			EMPLOYEE_HOURS=8
+		;;
+		$IS_PART_TIME)
+			EMPLOYEE_HOURS=4
+		;;
+		$IS_ABSENT)
+			EMPLOYEE_HOURS=0
+		;;
+		*)
+			printf "Invalid"
+		;;
+	esac
+	dailyWage=$(( $EMPLOYEE_HOURS*$EMPLOYEE_WAGE_PER_HOUR ))
+	monthlyWage=$(( $monthlyWage+$dailyWage ))
+done
