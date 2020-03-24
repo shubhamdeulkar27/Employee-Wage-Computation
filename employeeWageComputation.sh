e242c17dedcea82a1d7c1d86b042d1e35b9b0e7d
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-24 17:11:19 +0530

    Refactored: Using Case Statements for to Find Employee Working Hours

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -4,21 +4,24 @@
 IS_ABSENT=0
 IS_PART_TIME=1
 IS_FULL_TIME=2
-
 EMPLOYEE_WAGE_PER_HOUR=20
-FULL_TIME_HOURS=8
-PART_TIME_HOURS=4
-
 checkEmployee=$(( RANDOM%3 ))
 dailyWage=0
 
-if (( $checkEmployee==$IS_FULL_TIME ))
-then
-	dailyWage=$(( $FULL_TIME_HOURS*$EMPLOYEE_WAGE_PER_HOUR ))
-elif (( $checkEmployee==$IS_PART_TIME ))
-then
-	dailyWage=$(( $PART_TIME_HOURS*$EMPLOYEE_WAGE_PER_HOUR ))
-elif (( $checkEmployee==$IS_ABSENT ))
-then
-	dailyWage=0
-fi
+#CALCULATING DAILY WAGE
+case $checkEmployee in
+	$IS_FULL_TIME)
+		EMPLOYEE_HOURS=8
+	;;
+	$IS_PART_TIME)
+		EMPLOYEE_HOURS=4
+	;;
+	$IS_ABSENT)
+		EMPLOYEE_HOURS=0
+	;;
+	*)
+		printf "Invalid"
+	;;
+esac
+
+dailyWage=$(( $EMPLOYEE_HOURS*$EMPLOYEE_WAGE_PER_HOOUR ))
