19fa2d70ef6988045175f05753295df8c6dfdad1
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-24 16:59:37 +0530

    Refactored: Calculating Part Time Wage

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,15 +1,24 @@
 #!/bin/bash -x
 
 #CHECKING EMPLOYEE IS PRESENT
-IS_PRESENT=1
-EMPLOYEE_WAGE_PER_HOUR=20
-FULL_DAY_HOURS=8
+IS_ABSENT=0
+IS_PART_TIME=1
+IS_FULL_TIME=2
 
-checkEmployee=$(( RANDOM%2 ))
+EMPLOYEE_WAGE_PER_HOUR=20
+FULL_TIME_HOURS=8
+PART_TIME_HOURS=4
+
+checkEmployee=$(( RANDOM%3 ))
 dailyWage=0
-if (( $checkEmployee==$IS_PRESENT ))
+
+if (( $checkEmployee==$IS_FULL_TIME ))
 then
-	dailyWage=$(( $FULL_DAY_HOURS*$EMPLOYEE_WAGE_PER_HOUR ))
-else
-	printf "Employee Is Not Present\n"
+	dailyWage=$(( $FULL_TIME_HOURS*$EMPLOYEE_WAGE_PER_HOUR ))
+elif (( $checkEmployee==$IS_PART_TIME ))
+then
+	dailyWage=$(( $PART_TIME_HOURS*$EMPLOYEE_WAGE_PER_HOUR ))
+elif (( $checkEmployee==$IS_ABSENT ))
+then
+	dailyWage=0
 fi
